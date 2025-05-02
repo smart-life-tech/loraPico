afb2d4b93d1d0ac4eea16f09190891c389419243
smart-life-tech <christlightworld@gmail.com>
2025-05-02 14:28:51 +0100

    remoced spiclock

--- pocsag_transmitter/pocsag_transmitter.ino
+++ pocsag_transmitter/pocsag_transmitter.ino
@@ -146,7 +146,6 @@ void setup()
     SPI1.setCS(SX1262_CS);
     SPI1.setSCK(SX1262_SCK);
     SPI1.setTX(SX1262_MOSI);
-    SPI1.setClock(4000000); // 4 MHz clock speed
     SPI1.begin();
     Serial.println("SPI1 initialized");
     delay(1000);
